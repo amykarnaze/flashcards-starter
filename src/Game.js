c855f0cf32e5fe192ae43efa742c3b3d244098a1
Amy Karnaze <akarnaze@gmail.com>
2020-07-01 15:58:08 -0600

    Add currentRound property to Game class

--- src/Game.js
+++ src/Game.js
@@ -4,6 +4,7 @@ const util = require('./util');
 
 class Game {
   constructor() {}
+  this.currentRound = {};
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
