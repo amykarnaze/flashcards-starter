fdcc2a3f9ced519a9a24a65ecd9d9567d31c33bf
Amy Karnaze <akarnaze@gmail.com>
2020-06-30 21:15:11 -0600

    Add imports and beforeEach for test

---
+++ test/Deck-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+describe('Deck', function() {
+
+  let deck1, deck2, card1, card2, card3;
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    deck1 = new Deck([card1, card2, card3]);
+    deck2 = new Deck([card1, card2]);
+  });
